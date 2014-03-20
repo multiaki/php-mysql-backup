1f429b4dc2b364fc415da11e04c169d16908bfb5
Dimitrios Savvopoulos <ds@dimsav.com>
2014-03-20 08:09:33 +0100

    Removed implemented methods

--- src/Dimsav/Backup/Project.php
+++ src/Dimsav/Backup/Project.php
@@ -2,41 +2,13 @@
 
 class Project {
 
-    private $config;
+//    private $config;
 
-    private $name;
-    private $paths = array();
-    private $excludes = array();
-    private $password;
-
-    private $dbHost;
-    private $dbPort;
-    private $dbName;
-    private $dbUsername;
-    private $dbPassword;
-
+    // User this in the compressor
     private $generatedFiles = array();
 
-    public function __construct(Config $config, $projectName)
-    {
-        $this->config = $config;
 
-        $this->name     = $projectName;
-        $this->basePath = $this->getConfig('base-path');
-        $this->paths    = (array) $this->getConfig('paths');
-        $this->excludes = (array) $this->getConfig('excludes');
-        $this->password = $this->getConfig('password');
-        $this->dbName   = $this->getConfig("database.name");
-
-        if ($this->dbName)
-        {
-            $this->dbHost     = $this->getConfig('database.host');
-            $this->dbPort     = $this->getConfig('database.port');
-            $this->dbUsername = $this->getConfig('database.username');
-            $this->dbPassword = $this->getConfig('database.password');
-        }
-    }
-
+    // Use this in the compressor
     public function getBackupFile($extension = '')
     {
         $timestamp = date($this->config->get('app.timestamp_prefix', "Y.m.d.H.i."));
@@ -44,64 +16,17 @@ class Project {
         $file = $this->config->get('app.backups_dir');
         $file.= "/{$this->name}/{$timestamp}{$this->name}";
         $file.= $extension ? ".{$extension}" : '';
+
         return $file;
     }
 
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getBasePath()
-    {
-        return $this->basePath;
-    }
-
-    public function getPaths()
-    {
-        return $this->paths;
-    }
-
-    public function getExcludes()
-    {
-        return $this->excludes;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function getDbHost()
-    {
-        return $this->dbHost;
-    }
-
-    public function getDbPort()
-    {
-        return $this->dbPort;
-    }
-
-    public function getDbName()
-    {
-        return $this->dbName;
-    }
-
-    public function getDbUsername()
-    {
-        return $this->dbUsername;
-    }
-
-    public function getDbPassword()
-    {
-        return $this->dbPassword;
-    }
-
+    // User this in the compressor
     public function addToGeneratedFiles(array $files)
     {
         $this->generatedFiles = array_merge($this->generatedFiles, $files);
     }
 
+    // Use this in the parser
     private function getConfig($parameter)
     {
         return $this->config->get(
